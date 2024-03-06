95fb9b430b5f110d3d355fea0d4723f1a2365731
Saikat1 <saikatappsg4@gmail.com>
2024-03-06 18:45:50 +0600

    verson one

---
+++ app/src/main/java/com/appssaikat/myapplication/MainActivity.java
@@ -0,0 +1,35 @@
+package com.appssaikat.myapplication;
+
+import static androidx.core.widget.EdgeEffectCompat.getDistance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.ZoomControls;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity  {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+
+
+    }
+
+
